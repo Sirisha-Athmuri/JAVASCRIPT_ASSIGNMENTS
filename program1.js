c38694ae2286723c5ca021ad84d7aab66f326694
Sirisha-Athmuri <sirisha.8k@gmail.com>
2022-08-14 10:57:48 +0100

    assignment_1

---
+++ program1.js
@@ -0,0 +1,11 @@
+//Program1: Write a program to add two numbers and print the result to console.
+
+let a=2;
+let b=3;
+let result=a+b;
+console.log(result);
+
+function Mul(num1,num2,num3){
+    return num1*num2*num3
+}
+console.log(Mul(1,2,3));
